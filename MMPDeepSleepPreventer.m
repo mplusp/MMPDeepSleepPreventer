d286084f57e77b3677ee32a8fe749dbd3e2a6d73
Marco Peluso <marco@marcopeluso.com>
2011-01-01 20:38:44 +0100

    Use NSDate more appropriately and fix some formatting.

--- MMPDeepSleepPreventer.m
+++ MMPDeepSleepPreventer.m
@@ -113,11 +113,11 @@
 	
 	// We create a new repeating timer, that begins firing immediately and then every five seconds afterwards.
 	// Every time it fires, it calls -mmp_playPreventSleepSound.
-	NSTimer *preventSleepTimer = [[NSTimer alloc] initWithFireDate:[NSDate dateWithTimeIntervalSinceNow:0]
+	NSTimer *preventSleepTimer = [[NSTimer alloc] initWithFireDate:[NSDate date]
 	                                                      interval:5.0
 	                                                        target:self
 	                                                      selector:@selector(mmp_playPreventSleepSound)
-														  userInfo:nil
+	                                                      userInfo:nil
 	                                                       repeats:YES];
 	self.preventSleepTimer = preventSleepTimer;
 	[preventSleepTimer release];
@@ -149,12 +149,12 @@
 	// Initialize audio session
 	AudioSessionInitialize
 	(
-		NULL,	// Use NULL to use the default (main) run loop.
-		NULL,	// Use NULL to use the default run loop mode.
-		NULL,	// A reference to your interruption listener callback function.
-				// See “Responding to Audio Session Interruptions” in Apple's "Audio Session Programming Guide" for a description of how to write
-				// and use an interruption callback function.
-		NULL	// Data you intend to be passed to your interruption listener callback function when the audio session object invokes it.
+		NULL, // Use NULL to use the default (main) run loop.
+		NULL, // Use NULL to use the default run loop mode.
+		NULL, // A reference to your interruption listener callback function.
+		      // See “Responding to Audio Session Interruptions” in Apple's "Audio Session Programming Guide" for a description of how to write
+		      // and use an interruption callback function.
+		NULL  // Data you intend to be passed to your interruption listener callback function when the audio session object invokes it.
 	);
 	
 	// Activate audio session
@@ -168,13 +168,13 @@
 	
 	// Set up audio session category to kAudioSessionCategory_MediaPlayback.
 	// While playing sounds using this session category at least every 10 seconds, the iPhone doesn't go to sleep.
-	UInt32 sessionCategory = kAudioSessionCategory_MediaPlayback;	// Defines a new variable of type UInt32 and initializes it with the identifier 
-																	// for the category you want to apply to the audio session.
+	UInt32 sessionCategory = kAudioSessionCategory_MediaPlayback; // Defines a new variable of type UInt32 and initializes it with the identifier 
+	                                                              // for the category you want to apply to the audio session.
 	AudioSessionSetProperty
 	(
-		kAudioSessionProperty_AudioCategory,	// The identifier, or key, for the audio session property you want to set.
-		sizeof(sessionCategory),				// The size, in bytes, of the property value that you are applying.
-		&sessionCategory						// The category you want to apply to the audio session.
+		kAudioSessionProperty_AudioCategory, // The identifier, or key, for the audio session property you want to set.
+		sizeof(sessionCategory),             // The size, in bytes, of the property value that you are applying.
+		&sessionCategory                     // The category you want to apply to the audio session.
 	);
 	
 	// Set up audio session playback mixing behavior.
@@ -188,9 +188,9 @@
 	
 	propertySetError = AudioSessionSetProperty
 	(
-		kAudioSessionProperty_OverrideCategoryMixWithOthers,	// The identifier, or key, for the audio session property you want to set.
-		sizeof(allowMixing),									// The size, in bytes, of the property value that you are applying.
-		&allowMixing											// The value to apply to the property.
+		kAudioSessionProperty_OverrideCategoryMixWithOthers, // The identifier, or key, for the audio session property you want to set.
+		sizeof(allowMixing),                                 // The size, in bytes, of the property value that you are applying.
+		&allowMixing                                         // The value to apply to the property.
 	);
 	
 	if (propertySetError)
